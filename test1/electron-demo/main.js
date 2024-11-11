fdbb5d442b7af5ffdec66023221c8e1e745308ba
phanvanduyen01022002 <phanvanduyen01022002@gmail.com>
2024-11-11 12:00:33 +0700

    push test1 folder

---
+++ test1/electron-demo/main.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, "public", "index.html"));
+
+  win.on("closed", () => {
+    app.quit();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
